c939c57ceae2dd8014d59b51e416bf09e74ce4b2
BlackPrince163 <79138935+BlackPrince163@users.noreply.github.com>
2021-10-11 23:35:04 +0300

    + V3

---
+++ V3__fill_some_tables.sql
@@ -0,0 +1,10 @@
+INSERT INTO goods VALUES (1, 'water', 5, 3);
+INSERT INTO goods VALUES (2, 'milk', 3, 1);
+
+INSERT INTO "user" VALUES (1, 'Niyaz');
+INSERT INTO "user" VALUES (2, 'Marat');
+INSERT INTO "user" VALUES (3, 'Danil');
+INSERT INTO "user" VALUES (4, 'Dima');
+
+INSERT INTO reviews VALUES (1, 3, 'Очень хороший товар. Рекомендую');
+INSERT INTO reviews  VALUES (2, 2, 'Плохой товар. Не советую');
